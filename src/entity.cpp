1cd64de59765257275a6c189b479d463f09d835c
Ahn, Seung-rye <seungrye@devguru.co.kr>
2024-09-16 23:48:00 +0900

    format code

--- src/entity.cpp
+++ src/entity.cpp
@@ -1,84 +1,63 @@
 #include "entity.hpp"
-#include <iostream>
-#include <sys/time.h>
+#include <SFML/System/Thread.hpp>
 #include <ctime>
+#include <sys/time.h>
 
-Entity::Entity(const float &frame_index,
-               const float &animation_speed,
-               const py::Vector2f &direction,
-               const std::list<std::shared_ptr<SpriteTexture>> &obstacle_sprites)
-    : frame_index(frame_index),
-      animation_speed(animation_speed),
-      direction(direction),
-      obstacle_sprites(obstacle_sprites)
-{
+Entity::Entity(
+    const float &frame_index, const float &animation_speed,
+    const py::Vector2f &direction,
+    const std::list<std::shared_ptr<SpriteTexture>> &obstacle_sprites)
+    : frame_index(frame_index), animation_speed(animation_speed),
+      direction(direction), obstacle_sprites(obstacle_sprites) {}
+
+int Entity::wave_value() {
+  struct timeval time_now;
+  ::gettimeofday(&time_now, nullptr);
+  time_t msecs_time = (time_now.tv_sec * 1000) + (time_now.tv_usec / 1000);
+
+  auto value = sin(msecs_time);
+
+  if (value > 0.f)
+    return 255;
+  else
+    return 0;
 }
 
-int Entity::wave_value()
-{
-    struct timeval time_now;
-    ::gettimeofday(&time_now, nullptr);
-    time_t msecs_time = (time_now.tv_sec * 1000) + (time_now.tv_usec / 1000);
+void Entity::move(float speed) {
+  // move same speed in x and y direction
+  if (this->direction.magnitude() != 0) {
+    this->direction = this->direction.normalize();
+  }
 
-    auto value = sin(msecs_time);
+  this->hitbox_.transform(this->direction.x * speed, 0);
+  this->collision("horizontal");
+  this->hitbox_.transform(0, this->direction.y * speed);
+  this->collision("vertical");
 
-    if (value > 0.f)
-        return 255;
-    else
-        return 0;
+  this->rect_.center(this->hitbox_.center());
 }
 
-void Entity::move(float speed)
-{
-    // move same speed in x and y direction
-    if (this->direction.magnitude() != 0)
-    {
-        this->direction = this->direction.normalize();
-    }
-
-    this->hitbox_.transform(this->direction.x * speed, 0);
-    this->collision("horizontal");
-    this->hitbox_.transform(0, this->direction.y * speed);
-    this->collision("vertical");
-
-    this->rect_.center(this->hitbox_.center());
-}
-
-void Entity::collision(const std::string &direction)
-{
-    sf::Rect<float> intersection;
-    if (!direction.compare("horizontal"))
-    {
-        for (const auto &sprite : this->obstacle_sprites)
-        {
-            if (sprite->colliderect(this->hitbox_, intersection))
-            {
-                if (this->direction.x > 0)
-                { // move right
-                    this->hitbox_.transform(intersection.width * -1, 0);
-                }
-                else if (this->direction.x < 0)
-                { // move left
-                    this->hitbox_.transform(intersection.width, 0);
-                }
-            }
+void Entity::collision(const std::string &direction) {
+  sf::Rect<float> intersection;
+  if (!direction.compare("horizontal")) {
+    for (const auto &sprite : this->obstacle_sprites) {
+      if (sprite->colliderect(this->hitbox_, intersection)) {
+        if (this->direction.x > 0) { // move right
+          this->hitbox_.transform(intersection.width * -1, 0);
+        } else if (this->direction.x < 0) { // move left
+          this->hitbox_.transform(intersection.width, 0);
         }
+      }
     }
-    else if (!direction.compare("vertical"))
-    {
-        for (const auto &sprite : this->obstacle_sprites)
-        {
-            if (sprite->colliderect(this->hitbox_, intersection))
-            {
-                if (this->direction.y > 0)
-                { // move down
-                    this->hitbox_.transform(0, intersection.height * -1);
-                }
-                else if (this->direction.y < 0)
-                { // move up
-                    this->hitbox_.transform(0, intersection.height);
-                }
-            }
+  } else if (!direction.compare("vertical")) {
+    for (const auto &sprite : this->obstacle_sprites) {
+      if (sprite->colliderect(this->hitbox_, intersection)) {
+        if (this->direction.y > 0) { // move down
+          this->hitbox_.transform(0, intersection.height * -1);
+        } else if (this->direction.y < 0) { // move up
+          this->hitbox_.transform(0, intersection.height);
         }
+      }
     }
+  }
 }
--- src/level.cpp
+++ src/level.cpp
@@ -1,31 +1,26 @@
 #include "level.hpp"
+#include "enemy.hpp"
 #include "settings.hpp"
 #include "support.hpp"
 #include "tile.hpp"
-#include "enemy.hpp"
 #include "weapon.hpp"
 #include <SFML/System/Vector2.hpp>
-#include <algorithm>
 #include <cassert>
 #include <cstdlib>
-#include <iterator>
 #include <map>
+#include <memory>
 #include <string>
 #include <utility>
 #include <vector>
-#include <memory>
-#include <iostream>
 
 Level::Level()
     : game_paused(false),
       animation_player(std::make_shared<AnimationPlayer>(*this)),
-      magic_player(std::make_shared<MagicPlayer>(this->animation_player))
-{
+      magic_player(std::make_shared<MagicPlayer>(this->animation_player)) {
   this->create_map();
 }
 
-void Level::create_map()
-{
+void Level::create_map() {
   std::map<std::string, std::vector<std::vector<std::string>>> layouts = {
       {"boundary", import_csv_layout("./src/map/map_FloorBlocks.csv")},
       {"grass", import_csv_layout("./src/map/map_Grass.csv")},
@@ -33,22 +28,18 @@ void Level::create_map()
       {"entities", import_csv_layout("./src/map/map_Entities.csv")},
   };
 
-  std::map<std::string, std::vector<std::shared_ptr<SpriteTexture>>> graphics = {
-      {"grass", import_folder("./src/graphics/grass/")},
-      {"object", import_folder("./src/graphics/objects/")}};
+  std::map<std::string, std::vector<std::shared_ptr<SpriteTexture>>> graphics =
+      {{"grass", import_folder("./src/graphics/grass/")},
+       {"object", import_folder("./src/graphics/objects/")}};
 
-  for (const auto &entry : layouts)
-  {
+  for (const auto &entry : layouts) {
     auto style = entry.first;
     auto layout = entry.second;
     int row_index = 0;
-    for (const auto &row : layout)
-    {
+    for (const auto &row : layout) {
       int col_index = 0;
-      for (const auto &col : row)
-      {
-        if (!col.compare("-1"))
-        {
+      for (const auto &col : row) {
+        if (!col.compare("-1")) {
           col_index++;
           continue;
         }
@@ -56,49 +47,37 @@ void Level::create_map()
         auto x = col_index * TILESIZE;
         auto y = row_index * TILESIZE;
 
-        if (!style.compare("boundary"))
-        {
+        if (!style.compare("boundary")) {
           auto tile = std::make_shared<Tile>(sf::Vector2f(x, y), "invisible");
           this->obstacle_sprites.push_back(tile);
-        }
-        else if (!style.compare("grass"))
-        {
-          auto grass = this->random_choice<std::vector<std::shared_ptr<SpriteTexture>>>(graphics["grass"]);
-          auto tile = std::make_shared<Tile>(sf::Vector2f(x, y), "grass", *grass);
+        } else if (!style.compare("grass")) {
+          auto grass =
+              this->random_choice<std::vector<std::shared_ptr<SpriteTexture>>>(
+                  graphics["grass"]);
+          auto tile =
+              std::make_shared<Tile>(sf::Vector2f(x, y), "grass", *grass);
           this->visible_sprites.push_back(tile);
           this->obstacle_sprites.push_back(tile);
           this->attackable_sprites.push_back(tile);
-        }
-        else if (!style.compare("object"))
-        {
+        } else if (!style.compare("object")) {
           auto sprite = graphics["object"][std::stoi(col)];
-          auto tile = std::make_shared<Tile>(sf::Vector2f(x, y), "object", *sprite);
+          auto tile =
+              std::make_shared<Tile>(sf::Vector2f(x, y), "object", *sprite);
           this->visible_sprites.push_back(tile);
           this->obstacle_sprites.push_back(tile);
-        }
-        else if (!style.compare("entities"))
-        {
-          if (!col.compare("394"))
-          {
+        } else if (!style.compare("entities")) {
+          if (!col.compare("394")) {
             this->player = std::make_shared<Player>(
-                sf::Vector2f(x, y),
-                this->obstacle_sprites,
-                [this]() -> void
-                { this->create_attack(); },
-                [this]() -> void
-                {
-                  this->destroy_attack();
-                },
-                [this](const std::string &style, int strength, int cost)
-                {
+                sf::Vector2f(x, y), this->obstacle_sprites,
+                [this]() -> void { this->create_attack(); },
+                [this]() -> void { this->destroy_attack(); },
+                [this](const std::string &style, int strength, int cost) {
                   this->create_magic(style, strength, cost);
                 },
-                [this]() -> void
-                {
-                  this->destroy_magic();
-                },
-                [this](int amount, const std::string &attack_type) -> void
-                { this->damage_player(amount, attack_type); });
+                [this]() -> void { this->destroy_magic(); },
+                [this](int amount, const std::string &attack_type) -> void {
+                  this->damage_player(amount, attack_type);
+                });
             this->upgrade = std::make_shared<Upgrade>(this->player);
             this->visible_sprites.push_back(this->player);
           }
@@ -110,18 +89,14 @@ void Level::create_map()
     }
   }
 
-  for (const auto &entry : layouts)
-  {
+  for (const auto &entry : layouts) {
     auto style = entry.first;
     auto layout = entry.second;
     int row_index = 0;
-    for (const auto &row : layout)
-    {
+    for (const auto &row : layout) {
       int col_index = 0;
-      for (const auto &col : row)
-      {
-        if (!col.compare("-1"))
-        {
+      for (const auto &col : row) {
+        if (!col.compare("-1")) {
           col_index++;
           continue;
         }
@@ -129,12 +104,10 @@ void Level::create_map()
         auto x = col_index * TILESIZE;
         auto y = row_index * TILESIZE;
 
-        if (!style.compare("entities"))
-        {
+        if (!style.compare("entities")) {
           if (col.compare("394")) // not 394
           {
-            auto monster_name = [&col]() -> std::string
-            {
+            auto monster_name = [&col]() -> std::string {
               if (!col.compare("390"))
                 return "bamboo";
               if (!col.compare("391"))
@@ -145,28 +118,20 @@ void Level::create_map()
                 return "squid";
               assert(false);
             }();
-            if (!monster_name.empty())
-            {
+            if (!monster_name.empty()) {
               auto enemy = std::make_shared<Enemy>(
-                  monster_name,
-                  sf::Vector2f(x, y),
-                  this->obstacle_sprites,
-                  [this](const sf::Vector2f &pos, const std::string &monster_name)
-                  {
+                  monster_name, sf::Vector2f(x, y), this->obstacle_sprites,
+                  [this](const sf::Vector2f &pos,
+                         const std::string &monster_name) {
                     this->trigger_death_particles(pos, monster_name);
                   },
-                  [this](int exp)
-                  {
-                    this->add_exp(exp);
-                  },
-                  *this,
-                  this->player);
+                  [this](int exp) { this->add_exp(exp); }, *this, this->player);
               this->visible_sprites.push_back(enemy);
               this->attackable_sprites.push_back(enemy);
             }
           }
         }
-        
+
         col_index++;
       }
       row_index++;
@@ -174,98 +139,77 @@ void Level::create_map()
   }
 }
 
-void Level::trigger_death_particles(const sf::Vector2f &pos, const std::string &particle_type)
-{
+void Level::trigger_death_particles(const sf::Vector2f &pos,
+                                    const std::string &particle_type) {
   auto sprite = this->animation_player->create_particles(pos, particle_type);
   this->visible_sprites.push_back(sprite);
 }
 
-void Level::add_exp(int amount)
-{
+void Level::add_exp(int amount) {
   this->player->exp(this->player->exp() + amount);
 }
 
-void Level::create_attack()
-{
+void Level::create_attack() {
   this->current_attack = std::make_shared<Weapon>(this->player);
   this->visible_sprites.push_back(this->current_attack);
   this->attack_sprites.push_back(this->current_attack);
 }
 
-void Level::destroy_attack()
-{
-  if (this->current_attack)
-  {
+void Level::destroy_attack() {
+  if (this->current_attack) {
     this->kill(this->current_attack);
     this->current_attack = nullptr;
   }
 }
 
-void Level::create_magic(const std::string &style, int strength, int cost)
-{
-  if (!style.compare("heal"))
-  {
+void Level::create_magic(const std::string &style, int strength, int cost) {
+  if (!style.compare("heal")) {
     auto sprites = this->magic_player->heal(this->player, strength, cost);
-    for (const auto &sprite : sprites)
-    {
+    for (const auto &sprite : sprites) {
       this->visible_sprites.push_back(sprite);
     }
-  }
-  else if (!style.compare("flame"))
-  {
+  } else if (!style.compare("flame")) {
     auto sprites = this->magic_player->flame(this->player, cost);
-    for (const auto &sprite : sprites)
-    {
+    for (const auto &sprite : sprites) {
       this->visible_sprites.push_back(sprite);
       this->attack_sprites.push_back(sprite);
     }
   }
 }
 
-void Level::destroy_magic()
-{
-}
+void Level::destroy_magic() {}
 
-void Level::run()
-{
+void Level::run() {
   this->visible_sprites.custom_draw(player);
   this->ui.display(this->player);
 
-  if (this->game_paused)
-  {
+  if (this->game_paused) {
     this->upgrade->display();
-  }
-  else
-  {
+  } else {
     this->visible_sprites.update();
     this->player_attack_logic();
     this->remove_dead_sprites();
   }
 }
 
-void Level::player_attack_logic()
-{
-  for (const auto &attack_sprite : this->attack_sprites)
-  {
-    auto collision_sprites = ::spritecollide(attack_sprite, this->attackable_sprites);
-    for (const auto &collision_sprite : collision_sprites)
-    {
-      if (!collision_sprite->sprite_type().compare("grass"))
-      {
+void Level::player_attack_logic() {
+  for (const auto &attack_sprite : this->attack_sprites) {
+    auto collision_sprites =
+        ::spritecollide(attack_sprite, this->attackable_sprites);
+    for (const auto &collision_sprite : collision_sprites) {
+      if (!collision_sprite->sprite_type().compare("grass")) {
         auto rect = collision_sprite->rect();
         auto pos = rect.center();
         auto offset = py::Vector2f(0, 75);
         int range = 3 + (rand() % 4); // 3 ~ 6
-        for (auto i = 0; i < range; ++i)
-        {
-          auto particle = this->animation_player->create_grass_particles(pos - offset);
+        for (auto i = 0; i < range; ++i) {
+          auto particle =
+              this->animation_player->create_grass_particles(pos - offset);
           this->visible_sprites.push_back(particle);
         }
 
         this->kill(collision_sprite);
-      }
-      else if (!collision_sprite->sprite_type().compare("enemy"))
-      {
+      } else if (!collision_sprite->sprite_type().compare("enemy")) {
         auto enemy = std::static_pointer_cast<Enemy>(collision_sprite);
         enemy->get_damage(this->player, attack_sprite->sprite_type());
       }
@@ -273,10 +217,8 @@ void Level::player_attack_logic()
   }
 }
 
-void Level::damage_player(int amount, const std::string &attack_type)
-{
-  if (this->player->vulernable())
-  {
+void Level::damage_player(int amount, const std::string &attack_type) {
+  if (this->player->vulernable()) {
     this->player->health(this->player->health() - amount);
     this->player->vulernable(false);
     this->player->restart_hurt_time();
@@ -287,8 +229,7 @@ void Level::damage_player(int amount, const std::string &attack_type)
   }
 }
 
-YSortCameraGroup::YSortCameraGroup()
-{
+YSortCameraGroup::YSortCameraGroup() {
   auto &rnderer = GameWindow::instance();
   this->half_width = rnderer.screen().getSize().x / 2;
   this->half_height = rnderer.screen().getSize().y / 2;
@@ -299,25 +240,23 @@ YSortCameraGroup::YSortCameraGroup()
   assert(r);
 }
 
-void YSortCameraGroup::custom_draw(std::shared_ptr<Player> player)
-{
+void YSortCameraGroup::custom_draw(std::shared_ptr<Player> player) {
   this->offset.x = player->rect().centerx - this->half_width;
   this->offset.y = player->rect().centery - this->half_height;
 
   // drawing the floor
-  auto floor_offset_pos = sf::Vector2f(this->floor.rect().left - this->offset.x, this->floor.rect().top - this->offset.y);
+  auto floor_offset_pos = sf::Vector2f(this->floor.rect().left - this->offset.x,
+                                       this->floor.rect().top - this->offset.y);
   this->floor.set_origin(floor_offset_pos);
   GameWindow::instance().screen().draw(this->floor.surf());
 
-  this->sprites.sort(
-      [](auto a, auto b) -> bool
-      {
-        return a->rect().centery < b->rect().centery;
-      });
+  this->sprites.sort([](auto a, auto b) -> bool {
+    return a->rect().centery < b->rect().centery;
+  });
 
-  for (const auto &sprite : this->sprites)
-  {
-    auto offset_rect = sf::Vector2f(sprite->rect().left - this->offset.x, sprite->rect().top - this->offset.y);
+  for (const auto &sprite : this->sprites) {
+    auto offset_rect = sf::Vector2f(sprite->rect().left - this->offset.x,
+                                    sprite->rect().top - this->offset.y);
     sprite->set_origin(offset_rect);
     GameWindow::instance().screen().draw(sprite->surf());
 
@@ -346,10 +285,8 @@ void YSortCameraGroup::custom_draw(std::shared_ptr<Player> player)
   GameWindow::instance().screen().display();
 }
 
-void YSortCameraGroup::update()
-{
-  for (const auto &sprite : this->sprites)
-  {
+void YSortCameraGroup::update() {
+  for (const auto &sprite : this->sprites) {
     sprite->update();
   }
 }
